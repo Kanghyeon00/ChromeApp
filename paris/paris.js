1a7394af097af8d61cf58dfe3a81931f5725d0ee
seokhyeon kang <hyeony0419@naver.com>
2022-09-27 23:21:36 +0900

    first commit

---
+++ paris/paris.js
@@ -0,0 +1,6 @@
+document.querySelector('.next').addEventListener('click', function(){
+    document.querySelector('.main-img').style.transform = 'translate(-33.33vw)';
+});
+document.querySelector('.next').addEventListener('click', function(){
+    document.querySelector('.main-img').style.transform = 'translate(+66.66)';
+});
